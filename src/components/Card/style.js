0a00d58c332f4adc60469e5c8b97f2d77e88f132
= <=>
2020-02-24 16:04:09 -0500

    Fix card styles

--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -1,12 +1,15 @@
 export default (theme) => ({
   root: {
     maxWidth: 400,
+    [theme.breakpoints.up('lg')]: {
+      maxWidth: 500,
+    },
   },
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
   },
   avatar: {
-    backgroundColor: theme.palette.primary,
+    backgroundColor: theme.palette.primary.main,
   },
 });
