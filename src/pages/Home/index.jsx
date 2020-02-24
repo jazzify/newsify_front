967ab6f5edda01ea3c4de73a388ae1700742a7f6
= <=>
2020-02-24 16:04:50 -0500

    Enhance Home Page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../../components/Banner';
+import Category from '../../components/Category';
 
 function Home() {
   return (
     <>
       <Banner />
+      <Category />
+      <Category />
+      <Category />
     </>
   );
 }
